a2c7b62e96226cf96c499984eaa55d6a3c631ff7
vadukiya <vadukiyavidek@gmail.com>
2023-02-04 00:01:20 +0530

    folder upload chnages

---
+++ include/footer.php
@@ -0,0 +1,22 @@
+<footer class="section-padding">
+            <div class="container">
+                <div class="row">
+                    <div class="col col-md-8 col-xs-10 col-md-offset-2 col-xs-offset-1">
+                        <div class="box">
+                            <!-- frame -->
+                            <div class="left-top-border"></div> 
+                            <div class="right-top-border"></div> 
+                            <div class="bottom-right-border"></div> 
+                            <div class="bottom-left-border"></div> 
+                            <!-- frame -->
+
+                            <div class="love-birds wow fadeInSlow" style="visibility: visible; animation-name: fadeInSlow;"><i class="fi fa fa-heart"></i></div>
+                            <h2 class="wow fadeInSlow" style="visibility: visible; animation-name: fadeInSlow;">We are getting married</h2>
+                            <p class="wow fadeInSlow" style="visibility: visible; animation-name: fadeInSlow;">Jay &amp; Jayshree</p>
+                            <span class="wow fadeInSlow" style="visibility: visible; animation-name: fadeInSlow;">19 Feb 2018</span>
+                        </div>
+                        <p class="copyright">Website Designed by <a href="https://www.facebook.com/profile.php?id=100008469196654" target="_blank" style="color:#fff;"><strong style="color:#aa9a45;">Kishan Trambadiya. </strong></a></p>
+                  </div>
+                </div>
+            </div>
+        </footer>
---
+++ include/header.php
@@ -0,0 +1,42 @@
+
+            
+        
+         <header id="header">
+         
+            <nav class="navigation navbar navbar-default">
+                <div class="container">
+                    <div class="navbar-header">
+                    
+                        <button type="button" class="open-btn">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand logo-small" href="#"><img src="images/logo-v2.png" alt="" /></a>
+                    </div>
+                    <div id="navbar" class="navbar-collapse collapse navbar-right">
+                        <button class="close-navbar"><i class="fa fa-close"></i></button>
+                        <ul class="nav navbar-nav">
+                            <li class="mobile-menu-logo">Jay <i class="fa fa-heart"></i> Jayshree</li>
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <!--<li><a href="#couple">Couple</a></li>-->
+                            <li><a href="#story">Story</a></li>
+                            <li><a href="#wedding">Events</a></li>
+                           <!-- <li><a href="#people">People</a></li>-->
+                            <li><a href="#events">Location</a></li>
+                            <li><a href="gallery.php">Photo Gallery</a></li>
+                           
+                        </ul>
+                    </div><!-- end of nav-collapse -->
+                   
+                </div><!-- end of container -->
+            </nav>
+
+            <div class="logo-bottom-shape-wrapper container">
+                <div class="logo-bottom-shape wow fadeInDown" data-wow-delay="4s">
+                    <span>J <i class="fa fa-heart"></i> J</span>
+                </div>
+            </div>
+        </header>
+         
