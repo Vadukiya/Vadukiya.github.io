dedfd49059a16ada385fa6221bf52c488e865384
vadukiya <vadukiyavidek@gmail.com>
2023-02-05 22:21:38 +0530

    image changes and content

--- js/script.js
+++ js/script.js
@@ -207,7 +207,7 @@
         = BIGDAY COUNTDOWN
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2018/02/19 09:00:00', function(event) {
+        $('#clock').countdown('2023/02/22 09:00:00', function(event) {
             var $this = $(this).html(event.strftime(''
             + '<div class="box"><div>%D</div> <span>Days</span> </div>'
             + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
